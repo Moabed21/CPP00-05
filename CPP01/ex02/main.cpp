e950f2107fddc7c1dbdb13646dec026bc49fba04
Moath Abed <moathabed21@gmail.com>
2026-08-03 11:58:35 +0300

    ex02 finished

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+# include <string.h>
+
+int main()
+{
+    std::string variable = "HI THIS IS BRAIN";
+    std::string *stringPTR = &variable;
+    std::string &stringREF = variable;
+     
+    std::cout<<"string    address: "<<&variable<<std::endl;
+    std::cout<<"stringPTR address: "<<stringPTR<<std::endl;
+    std::cout<<"stringRED address: "<<&stringREF<<std::endl;
+
+    std::cout<<"string    value: "<<variable<<std::endl;
+    std::cout<<"stringPTR value: "<<*stringPTR<<std::endl;
+    std::cout<<"stringRED value: "<<stringREF<<std::endl;
+
+    return (0);
+}
